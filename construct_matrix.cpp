3f20bbed809fdac7fc389bc41c78a25ef6acbf9c
torstbje <torstbje@uio.no>
2022-09-16 16:42:33 +0200

    added function for constructing symmetric tridiagonal matrix with one fewer parameter

--- construct_matrix.cpp
+++ construct_matrix.cpp
@@ -1,20 +1,19 @@
 #include <armadillo>
 
-// Create a tridiagonal matrix tridiag(a,d,e) of size n*n,
-// from scalar input a, d, and e. That is, create a matrix where
-// - all n-1 elements on the subdiagonal have value a
-// - all n elements on the diagonal have value d
-// - all n-1 elements on the superdiagonal have value e
-arma::mat create_tridiagonal(int n, double a, double d, double e)
-{
-    // Makes n*n matrix and fills diagonal
+arma::mat create_tridiagonal(int n, double a, double d, double e) {
+    // Makes n*n matrix and fills the diagonal with input value d
     arma::mat A = arma::mat(n, n, arma::fill::eye)*d;
 
-    // Loop that sub- and superdiagonals
+    // Loop that sill the sub- and superdiagonals with a,e respectively
     for (int i = 0; i < n - 1; i++){
         A(i,i+1) = e;
         A(i+1,i) = a;
     }
 
     return A;
+}
+
+arma::mat create_tridiagonal_symmetric(int n, double a, double d) {
+    // Creates a symmetric tridiagonal matrix with create_tridiagonal as a 'parent' function
+    return create_tridiagonal(n,a,d,a);
 }
--- construct_matrix.hpp
+++ construct_matrix.hpp
@@ -1,6 +1,7 @@
 #ifndef CONSTRUCT_MATRIX_HPP
 #define CONSTRUCT_MATRIX_HPP
 
-arma::mat create_tridiagonal(int,double,double,double);
+arma::mat create_tridiagonal(int, double, double, double);
+arma::mat create_tridiagonal_symmetric(int, double, double);
 
 #endif
