0d08f8930f2e0d1f6cd5b1b8d1311c7f0a42d302
KlogeBukser <imvacant0923@gmail.com>
2022-09-22 12:42:50 +0200

    rmed

--- tests.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef TESTS_HPP
-#define TESTS_HPP
-
-bool test_max_offdiag_symmetric();
-bool test_tridiagonal_construction();
-bool test_jacobi_eigensolver();
-bool compare_with_analytical(const arma::mat&, const arma::vec&, const double&, const double&);
-
-#endif
